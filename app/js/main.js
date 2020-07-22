5e72658343cf3d5391e97f142dbdd89984eeeb52
zora0412 <resttsg@gmail.com>
2020-07-22 18:14:03 +0300

    add product page

--- app/js/main.js
+++ app/js/main.js
@@ -10,8 +10,29 @@ $(function(){
       arrows: false,
       slidesToShow: 4,
       slidesToScroll: 4
-    });  
+    });
+    
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function(){
+      $('.flip').addClass('list');
+      $('.icon-th-list').addClass('active');
+      $('.icon-th-large').removeClass('active')
+    });
+
+    $('.icon-th-large').on('click', function(){
+      $('.flip').removeClass('list');
+      $('.icon-th-large').addClass('active');
+      $('.icon-th-list').removeClass('active');
+    });
 
     var mixer = mixitup('.products__inner-box');
-
+    
 });
